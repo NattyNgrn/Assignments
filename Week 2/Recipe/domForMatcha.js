2012c69a2ca13042cf702ae99431f0350211f5a9
NattyNgrn <natnrgn@gmail.com>
2023-07-28 10:41:50 -0400

    appending

--- Week 2/Recipe/domForMatcha.js
+++ Week 2/Recipe/domForMatcha.js
@@ -5,4 +5,6 @@ let div = document.querySelector(".btn");
 let btn = document.createElement("button");
 btn.innerHTML = "<h2>Strawberry Milk Matcha!</h2>";
 btn.style.backgroundColor = "pink"
+div.appendChild(btn);
+
 
