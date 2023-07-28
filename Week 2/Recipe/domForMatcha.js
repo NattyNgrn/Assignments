59ec253851dcd8eaf098706d6e9104fd1436b2d4
NattyNgrn <natnrgn@gmail.com>
2023-07-28 10:41:21 -0400

    btn color

--- Week 2/Recipe/domForMatcha.js
+++ Week 2/Recipe/domForMatcha.js
@@ -4,3 +4,5 @@ let body = document.getElementsByName("body");
 let div = document.querySelector(".btn");
 let btn = document.createElement("button");
 btn.innerHTML = "<h2>Strawberry Milk Matcha!</h2>";
+btn.style.backgroundColor = "pink"
+
