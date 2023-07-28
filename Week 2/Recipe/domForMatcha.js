f92b0887a26bd2b107f8d7da96aa9aeb7e8f7373
NattyNgrn <natnrgn@gmail.com>
2023-07-28 10:40:45 -0400

    btn2

--- Week 2/Recipe/domForMatcha.js
+++ Week 2/Recipe/domForMatcha.js
@@ -3,3 +3,4 @@
 let body = document.getElementsByName("body");
 let div = document.querySelector(".btn");
 let btn = document.createElement("button");
+btn.innerHTML = "<h2>Strawberry Milk Matcha!</h2>";
